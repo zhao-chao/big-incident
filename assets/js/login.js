8d6a52a004f63fd6398b7daa3a4c7ccedd1cb8ea
/配置姓名 <1299898126@qq.com>
2020-10-30 16:26:41 +0800

    06_发起登录的Ajax请求

--- assets/js/login.js
+++ assets/js/login.js
@@ -58,4 +58,26 @@ $(function () {
 			$('#link_login').click()
 		})
 	})
+
+	// 监听登录表单的提交事件
+	$('#form_login').submit(function (e) {
+		// 阻止默认提交行为
+		e.preventDefault()
+		$.ajax({
+			url: 'http://ajax.frontend.itheima.net/api/login',
+			method: 'POST',
+			// 快速获取表单中的数据
+			data: $(this).serialize(),
+			success: function (res) {
+				if (res.status !== 0) {
+					return layer.msg('登录失败！')
+				}
+				layer.msg('登录成功！')
+				// 将登录成功得到的 token 字符串，保存到 localStorage 中
+				localStorage.setItem('token', res.token)
+				// 跳转到后台主页
+				location.href = './index.html'
+			},
+		})
+	})
 })
