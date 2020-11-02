2df2953ed4dc51817224ee64f1a8eb66602ef5c7
/配置姓名 <1299898126@qq.com>
2020-11-02 13:55:18 +0800

    13_

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,4 +1,6 @@
 $(function () {
+	var layer = layui.layer
+
 	// 1.1 获取裁剪区域的 DOM 元素
 	var $image = $('#image')
 	// 1.2 配置选项
@@ -12,6 +14,7 @@ $(function () {
 	// 1.3 创建裁剪区域
 	$image.cropper(options)
 
+	// 为上传按钮绑定点击事件
 	$('#btnChooseImage').on('click', function () {
 		$('#file').click()
 	})
@@ -33,5 +36,32 @@ $(function () {
 			.cropper('destroy') // 销毁旧的裁剪区域
 			.attr('src', imgURL) // 重新设置图片路径
 			.cropper(options) // 重新初始化裁剪区域
+	})
+
+	// 为确定按钮，绑定点击事件
+	$('#btnUpload').on('click', function () {
+		// 1. 要拿到用户裁剪之后的头像
+		var dataURL = $image
+			.cropper('getCroppedCanvas', {
+				// 创建一个 Canvas 画布
+				width: 100,
+				height: 100,
+			})
+			.toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+		// 2. 调用接口，把头像上传到服务器
+		$.ajax({
+			method: 'POST',
+			url: '/my/update/avatar',
+			data: {
+				avatar: dataURL,
+			},
+			success: function (res) {
+				if (res.status !== 0) {
+					return layer.msg('更换头像失败！')
+				}
+				layer.msg('更换头像成功！')
+				window.parent.getUserInfo()
+			},
+		})
 	})
 })
